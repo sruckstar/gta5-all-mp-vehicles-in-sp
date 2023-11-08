cd1cbd2f01c07d6c93b79adbd2d13a8932965f8e
sruckstar <sruckstargames@gmail.com>
2023-11-09 01:15:59 +0300

    DLC cars are now found in general traffic

--- All MP Vehicles in SP/industrial.cs
+++ All MP Vehicles in SP/industrial.cs
@@ -20,17 +20,30 @@ public class industrial : Script
     private float distance = 150.0f;
     private Blip marker;
 
+    private int street_flag = 1;
+    private int street_spawned = 0;
+    private float street_angle = 0.0f;
+    private float street_speed = 40.0f;
+    private Ped street_driver;
+    private Vehicle street_car;
+    private Vector3 street_coords = Vector3.Zero;
+    private int street_blip = 0;
+
+    private static int cars_number = 7; // To add a new vehicle, change this number
+
     private Vector3[] coords = new Vector3[4];
     private float[] angle = new float[4];
     private GTA.Vehicle car;
     private int all_coords;
-    private VehicleHash[] models = new VehicleHash[7];
+    private VehicleHash[] models = new VehicleHash[cars_number];
 
     public industrial()
     {
         config = ScriptSettings.Load("Scripts\\AllMpVehiclesInSp.ini");
         doors_config = config.GetValue<int>("MAIN", "doors", 1);
         blip_config = config.GetValue<int>("MAIN", "blips", 1);
+        street_flag = config.GetValue<int>("MAIN", "spawn_traffic", 1);
+        street_blip = config.GetValue<int>("MAIN", "traffic_cars_blips", 0);
 
         coords[0] = new Vector3(1566.097f, -1683.17f, 87.205f); 
         coords[1] = new Vector3(2673.478f, 1678.569f, 23.488f); 
@@ -56,6 +69,74 @@ public class industrial : Script
         Tick += OnTick;
     }
 
+    public enum Nodetype
+    {
+        AnyRoad,
+        Road,
+        Offroad,
+        Water
+    }
+
+    void GenerateSpawnPos(Vector3 desiredPos, Nodetype roadtype)
+    {
+        bool ForceOffroad = false;
+        OutputArgument outArgA = new OutputArgument();
+        OutputArgument outArgB = new OutputArgument();
+        int NodeNumber = 1;
+        int type = 0;
+
+        if (roadtype == Nodetype.AnyRoad)
+        {
+            type = 1;
+        }
+        if (roadtype == Nodetype.Road)
+        {
+            type = 0;
+        }
+        if (roadtype == Nodetype.Offroad)
+        {
+            type = 1;
+            ForceOffroad = true;
+        }
+        if (roadtype == Nodetype.Water)
+        {
+            type = 3;
+        }
+
+        int NodeID = Function.Call<int>(Hash.GET_NTH_CLOSEST_VEHICLE_NODE_ID_WITH_HEADING, desiredPos.X, desiredPos.Y, desiredPos.Z, NodeNumber, outArgA, outArgB, type, 300f, 300f);
+        if (ForceOffroad)
+        {
+            while (!Function.Call<bool>(Hash.GET_VEHICLE_NODE_IS_SWITCHED_OFF, NodeID) && NodeNumber < 500)
+            {
+                NodeNumber++;
+                NodeID = Function.Call<int>(Hash.GET_NTH_CLOSEST_VEHICLE_NODE_ID_WITH_HEADING, desiredPos.X, desiredPos.Y, desiredPos.Z, NodeNumber, outArgA, outArgB, type, 300f, 300f);
+            }
+        }
+
+        Function.Call(Hash.GET_VEHICLE_NODE_POSITION, NodeID, outArgA);
+        street_coords = outArgA.GetResult<Vector3>();
+        street_angle = outArgB.GetResult<float>();
+        Random rnd = new Random();
+        var veh_model = new Model(models[rnd.Next(0, cars_number)]);
+        veh_model.Request(500);
+        while (!veh_model.IsLoaded) Script.Wait(100);
+        street_car = World.CreateVehicle(veh_model, street_coords, street_angle);
+        Function.Call(Hash.DECOR_SET_INT, car, "MPBitset", 0);
+        street_spawned = 1;
+
+        street_driver = Function.Call<Ped>(Hash.CREATE_RANDOM_PED_AS_DRIVER, street_car, true);
+        street_driver.Task.CruiseWithVehicle(street_car, street_speed, DrivingStyle.Normal);
+
+        if (street_blip == 1)
+        {
+            marker = GTA.Native.Function.Call<Blip>(GTA.Native.Hash.ADD_BLIP_FOR_ENTITY, street_car);
+            GTA.Native.Function.Call(GTA.Native.Hash.SET_BLIP_SPRITE, marker, 1);
+            GTA.Native.Function.Call(GTA.Native.Hash.SET_BLIP_COLOUR, marker, 3);
+            GTA.Native.Function.Call(GTA.Native.Hash.FLASH_MINIMAP_DISPLAY);
+            marker.Name = "Unique vehicle";
+        }
+    }
+
     void OnTick(object sender, EventArgs e)
     {
         {
@@ -69,12 +150,18 @@ public class industrial : Script
                     if (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, coords[i].X, coords[i].Y, coords[i].Z, position.X, position.Y, position.Z, 0) < distance)
                     {
                         Random rnd = new Random();
-                        var veh_model = new Model(models[rnd.Next(0, 7)]);
+                        var veh_model = new Model(models[rnd.Next(0, cars_number)]);
                         veh_model.Request(500);
                         while (!veh_model.IsLoaded) Script.Wait(100);
                         car = World.CreateVehicle(veh_model, coords[i], angle[i]);
                         Function.Call(Hash.DECOR_SET_INT, car, "MPBitset", 0);
                         spawned = 1;
+
+                        if (street_flag == 1 && street_spawned == 0)
+                        {
+                            GenerateSpawnPos(coords[i], Nodetype.Road);
+                            street_spawned = 1;
+                        }
 
                         if (blip_config == 1)
                         {
@@ -97,28 +184,36 @@ public class industrial : Script
                 }
             }
 
-            
+
+            //If the player gets in the car, we clear it from memory
             if (car != null)
             {
                 if (GTA.Native.Function.Call<bool>(GTA.Native.Hash.IS_PED_IN_VEHICLE, Game.Player.Character, car, false))
                 {
-                    if (doors_config == 1)
-                    {
-                        GTA.Native.Function.Call(GTA.Native.Hash.SET_VEHICLE_ALARM, car, true);
-                        GTA.Native.Function.Call(GTA.Native.Hash.START_VEHICLE_ALARM, car);
-                    }
-
                     if (blip_config == 1)
                     {
                         marker.Delete();
                     }
-
                     car.MarkAsNoLongerNeeded();
                     car = null;
                     position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
                 }
             }
-
+            //Similarly for the car from traffic
+            if (street_car != null)
+            {
+                if (GTA.Native.Function.Call<bool>(GTA.Native.Hash.IS_PED_IN_VEHICLE, Game.Player.Character, street_car, false))
+                {
+                    if (blip_config == 1)
+                    {
+                        marker.Delete();
+                    }
+                    street_car.MarkAsNoLongerNeeded();
+                    street_car = null;
+                    position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                }
+            }
+            //If the car is deleted and the player has left the stream zone, you can spawn a new car.
             if (car == null && spawned == 1)
             {
                 position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
@@ -129,9 +224,21 @@ public class industrial : Script
                 }
                 spawned = 0;
             }
-
-
-            if (spawned == 1 && car != null) 
+            //Position and old_position coordinates are used for cars from traffic. 
+            //old_position has static coordinates, position changes every 100 ms.
+            if (street_car == null && street_spawned == 1)
+            {
+                position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                Vector3 old_position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                while (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, old_position.X, old_position.Y, old_position.Z, position.X, position.Y, position.Z, 0) < distance)
+                {
+                    Script.Wait(100);
+                    position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                }
+                street_spawned = 0;
+            }
+            //If the car exists but the player is far away, delete the car
+            if (spawned == 1 && car != null)
             {
                 position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
                 if (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, coords[x].X, coords[x].Y, coords[x].Z, position.X, position.Y, position.Z, 0) > distance)
@@ -142,6 +249,26 @@ public class industrial : Script
                     }
                     car.Delete();
                     car = null;
+                }
+            }
+            //Similarly for the car from traffic
+            if (street_spawned == 1 && street_car != null)
+            {
+                position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                if (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, street_car.Position.X, street_car.Position.Y, street_car.Position.Z, position.X, position.Y, position.Z, 0) > 500)
+                {
+                    if (marker != null)
+                    {
+                        marker.Delete();
+                    }
+
+                    if (street_driver != null)
+                    {
+                        street_driver.Delete();
+                    }
+
+                    street_car.Delete();
+                    street_car = null;
                 }
             }
         }
--- All MP Vehicles in SP/suvs.cs
+++ All MP Vehicles in SP/suvs.cs
@@ -22,11 +22,22 @@ public class Suvs : Script
     private float distance = 150.0f;
     private Blip marker;
 
+    private int street_flag = 1;
+    private int street_spawned = 0;
+    private float street_angle = 0.0f;
+    private float street_speed = 40.0f;
+    private Ped street_driver;
+    private Vehicle street_car;
+    private Vector3 street_coords = Vector3.Zero;
+    private int street_blip = 0;
+
+    private static int cars_number = 18; // To add a new vehicle, change this number
+
     private Vector3[] coords = new Vector3[8];
     private float[] angle = new float[8];
     private GTA.Vehicle car;
     private int all_coords;
-    private VehicleHash[] models = new VehicleHash[18];
+    private VehicleHash[] models = new VehicleHash[cars_number];
 
     public Suvs()
     {
@@ -34,6 +45,8 @@ public class Suvs : Script
         doors_config = config.GetValue<int>("MAIN", "doors", 1);
         blip_config = config.GetValue<int>("MAIN", "blips", 1);
         tuning_flag = config.GetValue<int>("MAIN", "tuning", 1);
+        street_flag = config.GetValue<int>("MAIN", "spawn_traffic", 1);
+        street_blip = config.GetValue<int>("MAIN", "traffic_cars_blips", 0);
 
         coords[0] = new Vector3(-2340.76f, 296.197f, 168.467f);
         coords[1] = new Vector3(629.014f, 196.173f, 96.128f);
@@ -76,6 +89,74 @@ public class Suvs : Script
         Tick += OnTick;
     }
 
+    public enum Nodetype
+    {
+        AnyRoad,
+        Road,
+        Offroad,
+        Water
+    }
+
+    void GenerateSpawnPos(Vector3 desiredPos, Nodetype roadtype)
+    {
+        bool ForceOffroad = false;
+        OutputArgument outArgA = new OutputArgument();
+        OutputArgument outArgB = new OutputArgument();
+        int NodeNumber = 1;
+        int type = 0;
+
+        if (roadtype == Nodetype.AnyRoad)
+        {
+            type = 1;
+        }
+        if (roadtype == Nodetype.Road)
+        {
+            type = 0;
+        }
+        if (roadtype == Nodetype.Offroad)
+        {
+            type = 1;
+            ForceOffroad = true;
+        }
+        if (roadtype == Nodetype.Water)
+        {
+            type = 3;
+        }
+
+        int NodeID = Function.Call<int>(Hash.GET_NTH_CLOSEST_VEHICLE_NODE_ID_WITH_HEADING, desiredPos.X, desiredPos.Y, desiredPos.Z, NodeNumber, outArgA, outArgB, type, 300f, 300f);
+        if (ForceOffroad)
+        {
+            while (!Function.Call<bool>(Hash.GET_VEHICLE_NODE_IS_SWITCHED_OFF, NodeID) && NodeNumber < 500)
+            {
+                NodeNumber++;
+                NodeID = Function.Call<int>(Hash.GET_NTH_CLOSEST_VEHICLE_NODE_ID_WITH_HEADING, desiredPos.X, desiredPos.Y, desiredPos.Z, NodeNumber, outArgA, outArgB, type, 300f, 300f);
+            }
+        }
+
+        Function.Call(Hash.GET_VEHICLE_NODE_POSITION, NodeID, outArgA);
+        street_coords = outArgA.GetResult<Vector3>();
+        street_angle = outArgB.GetResult<float>();
+        Random rnd = new Random();
+        var veh_model = new Model(models[rnd.Next(0, cars_number)]);
+        veh_model.Request(500);
+        while (!veh_model.IsLoaded) Script.Wait(100);
+        street_car = World.CreateVehicle(veh_model, street_coords, street_angle);
+        Function.Call(Hash.DECOR_SET_INT, car, "MPBitset", 0);
+        street_spawned = 1;
+
+        street_driver = Function.Call<Ped>(Hash.CREATE_RANDOM_PED_AS_DRIVER, street_car, true);
+        street_driver.Task.CruiseWithVehicle(street_car, street_speed, DrivingStyle.Normal);
+
+        if (street_blip == 1)
+        {
+            marker = GTA.Native.Function.Call<Blip>(GTA.Native.Hash.ADD_BLIP_FOR_ENTITY, street_car);
+            GTA.Native.Function.Call(GTA.Native.Hash.SET_BLIP_SPRITE, marker, 1);
+            GTA.Native.Function.Call(GTA.Native.Hash.SET_BLIP_COLOUR, marker, 3);
+            GTA.Native.Function.Call(GTA.Native.Hash.FLASH_MINIMAP_DISPLAY);
+            marker.Name = "Unique vehicle";
+        }
+    }
+
     void OnTick(object sender, EventArgs e)
     {
         {
@@ -89,12 +170,18 @@ public class Suvs : Script
                     if (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, coords[i].X, coords[i].Y, coords[i].Z, position.X, position.Y, position.Z, 0) < distance)
                     {
                         Random rnd = new Random();
-                        var veh_model = new Model(models[rnd.Next(0, 18)]);
+                        var veh_model = new Model(models[rnd.Next(0, cars_number)]);
                         veh_model.Request(500);
                         while (!veh_model.IsLoaded) Script.Wait(100);
                         car = World.CreateVehicle(veh_model, coords[i], angle[i]);
                         Function.Call(Hash.DECOR_SET_INT, car, "MPBitset", 0);
                         spawned = 1;
+
+                        if (street_flag == 1 && street_spawned == 0)
+                        {
+                            GenerateSpawnPos(coords[i], Nodetype.Road);
+                            street_spawned = 1;
+                        }
 
                         if (blip_config == 1)
                         {
@@ -166,17 +253,12 @@ public class Suvs : Script
                 }
             }
 
-            
+
+            //If the player gets in the car, we clear it from memory
             if (car != null)
             {
                 if (GTA.Native.Function.Call<bool>(GTA.Native.Hash.IS_PED_IN_VEHICLE, Game.Player.Character, car, false))
                 {
-                    if (doors_config == 1)
-                    {
-                        GTA.Native.Function.Call(GTA.Native.Hash.SET_VEHICLE_ALARM, car, true);
-                        GTA.Native.Function.Call(GTA.Native.Hash.START_VEHICLE_ALARM, car);
-                    }
-
                     if (blip_config == 1)
                     {
                         marker.Delete();
@@ -186,7 +268,21 @@ public class Suvs : Script
                     position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
                 }
             }
-
+            //Similarly for the car from traffic
+            if (street_car != null)
+            {
+                if (GTA.Native.Function.Call<bool>(GTA.Native.Hash.IS_PED_IN_VEHICLE, Game.Player.Character, street_car, false))
+                {
+                    if (blip_config == 1)
+                    {
+                        marker.Delete();
+                    }
+                    street_car.MarkAsNoLongerNeeded();
+                    street_car = null;
+                    position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                }
+            }
+            //If the car is deleted and the player has left the stream zone, you can spawn a new car.
             if (car == null && spawned == 1)
             {
                 position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
@@ -197,9 +293,21 @@ public class Suvs : Script
                 }
                 spawned = 0;
             }
-
-
-            if (spawned == 1 && car != null) 
+            //Position and old_position coordinates are used for cars from traffic. 
+            //old_position has static coordinates, position changes every 100 ms.
+            if (street_car == null && street_spawned == 1)
+            {
+                position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                Vector3 old_position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                while (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, old_position.X, old_position.Y, old_position.Z, position.X, position.Y, position.Z, 0) < distance)
+                {
+                    Script.Wait(100);
+                    position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                }
+                street_spawned = 0;
+            }
+            //If the car exists but the player is far away, delete the car
+            if (spawned == 1 && car != null)
             {
                 position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
                 if (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, coords[x].X, coords[x].Y, coords[x].Z, position.X, position.Y, position.Z, 0) > distance)
@@ -210,6 +318,26 @@ public class Suvs : Script
                     }
                     car.Delete();
                     car = null;
+                }
+            }
+            //Similarly for the car from traffic
+            if (street_spawned == 1 && street_car != null)
+            {
+                position = Game.Player.Character.GetOffsetPosition(new Vector3(0, 0, 0));
+                if (Function.Call<float>(Hash.GET_DISTANCE_BETWEEN_COORDS, street_car.Position.X, street_car.Position.Y, street_car.Position.Z, position.X, position.Y, position.Z, 0) > 500)
+                {
+                    if (marker != null)
+                    {
+                        marker.Delete();
+                    }
+
+                    if (street_driver != null)
+                    {
+                        street_driver.Delete();
+                    }
+
+                    street_car.Delete();
+                    street_car = null;
                 }
             }
         }
