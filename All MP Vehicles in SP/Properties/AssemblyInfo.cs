eae5edf6d9ee19b7118df7b0126a12f6ae472434
Michael21107 <121039033+Michael21107@users.noreply.github.com>
2025-06-03 19:17:29 +0200

    Update AssemblyInfo.cs

--- All MP Vehicles in SP/Properties/AssemblyInfo.cs
+++ All MP Vehicles in SP/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.2")]
-[assembly: AssemblyFileVersion("3.0.2")]
+[assembly: AssemblyVersion("3.0.4")]
+[assembly: AssemblyFileVersion("3.0.4")]
